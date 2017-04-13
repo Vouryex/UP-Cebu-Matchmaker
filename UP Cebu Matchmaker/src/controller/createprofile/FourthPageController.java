d23fec230ca589671c4f86b2704765792f88c7c0
Gamaliel Petralba <petralbagamaliel@gmail.com>
2017-04-14 00:17:56 +0800

    Upload picture

--- UP Cebu Matchmaker/src/controller/createprofile/FourthPageController.java
+++ UP Cebu Matchmaker/src/controller/createprofile/FourthPageController.java
@@ -1,12 +1,20 @@
 package controller.createprofile;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javax.imageio.ImageIO;
+
+import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
 
 public class FourthPageController implements Initializable {
 
@@ -16,6 +24,14 @@ public class FourthPageController implements Initializable {
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 				
+	}
+	
+	public void uploadPic() throws IOException {
+		FileChooser fileChooser = new FileChooser();
+		File selectedFile = fileChooser.showOpenDialog(null);
+		BufferedImage bufferedProfile = ImageIO.read(selectedFile);
+		Image profile = SwingFXUtils.toFXImage(bufferedProfile, null);
+		profilePicture.setImage(profile);
 	}
 	
 }
