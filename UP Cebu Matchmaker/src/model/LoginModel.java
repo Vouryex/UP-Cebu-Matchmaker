3d1026b0e41eeee93523f8a51776149820509408
Enrico <jerome.requioma@gmail.com>
2017-04-14 16:34:14 +0800

    profile displays name and age already

--- UP Cebu Matchmaker/src/model/LoginModel.java
+++ UP Cebu Matchmaker/src/model/LoginModel.java
@@ -1,8 +1,10 @@
 package model;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 import main.SqLiteConnection;
+import main.UserData;
 
 public class LoginModel {
 	Connection connection;
@@ -27,7 +29,7 @@ public class LoginModel {
 	public boolean isLogin(String user, String pass) throws SQLException {
 		PreparedStatement preparedStatement = null;
 		ResultSet resultSet = null;
-		String query = "SELECT * FROM user WHERE username = ? AND password = ?";
+		String query = "SELECT * FROM user WHERE lower(username) = lower(?) AND password = ?";
 		try {
 			preparedStatement = connection.prepareStatement(query);
 			preparedStatement.setString(1, user);
@@ -45,5 +47,26 @@ public class LoginModel {
 			preparedStatement.close();
 			resultSet.close();
 		}
+	}
+	
+	public ArrayList<String> getInfo(String user) {
+		ArrayList<String> info = new ArrayList<String>();
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+		String query = "SELECT * FROM user WHERE username = ?";
+		try {
+			preparedStatement = connection.prepareStatement(query);
+			preparedStatement.setString(1, user);
+			resultSet = preparedStatement.executeQuery();
+			info.add(resultSet.getString("firstname"));
+			info.add(resultSet.getString("surname"));
+			info.add(((Integer) resultSet.getInt("age")).toString());
+			info.add(resultSet.getString("username"));
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return info;
+		
 	}
 }
